fb93e5e8cdea9ba594b347e2fc8abe8f17d77b4b
Sean Martin <martins7@tcd.ie>
2020-02-28 13:18:56 +0000

    Closer to finding the right files...

--- Axona/get_files_from_list.py
+++ Axona/get_files_from_list.py
@@ -133,31 +133,97 @@ def find_files(info, data_dir):
     files = get_all_files_in_dir(
         data_dir, ext="set", verbose=False, recursive=True)
     good_files = []
+    tetrode_list = []
+    good_txt_files = []
     good_basenames = []
+    good_spike_files = []
+    units = []
+
     def ok_file(f):
-        for fname in info["FileName"]:
-            if os.path.basename(f)[:-4] == fname:
-                return True
-        return False
-    for f in files:
-        if ok_file(f):
-            good_files.append(f)
-            good_basenames.append(os.path.basename(f)[:-4])
-    return good_basenames
+        for fname in files:
+            if f == os.path.basename(fname[:-4]):
+                return True, fname
+            return False
+
+    # TODO need to match the rat name
+
+    # Find set files that match    
+    for i, f in enumerate(info["FileName"]):
+        test, fname = ok_file(f)
+        if test:
+            good_files.append(fname)
+            good_basenames.append(os.path.basename(fname)[:-4])
+            tetrode_list.append(info["Tetrode"][i])
+            units.append(info["Unit"][i])
+    txt_files = get_all_files_in_dir(
+        data_dir, ext="txt", verbose=False, recursive=True)
+
+    # Find txt files that match
+    for filename in good_files:
+        filename = filename[:-4]
+        for fname in txt_files:
+            if fname[:(len(filename) + 1)] == filename + "_":
+                pos_name = fname
+                good_txt_files.append(pos_name)
+                break
+
+    # Find cut files that match
+    for fname, tetrode in zip(good_files, tetrode_list):
+        filename = fname[:-4]
+        spike_name = filename + '.' + str(tetrode)
+        cut_name = filename + '_' + str(tetrode) + ".cut"
+        clu_name = filename + ".clu." + str(tetrode)
+        # print(filename+"\n" + spike_name+"\n" + cut_name+"\n" + clu_name+"\n")
+        if not os.path.isfile(spike_name):
+            continue
+            # Don't consider files that have not been clustered
+        if not (os.path.isfile(cut_name) or os.path.isfile(clu_name)):
+            print(
+                "Skipping tetrode {} - no cluster file named {} or {}".format(tetrode, cut_name, os.path.basename(clu_name)))
+            continue
+        good_spike_files.append(spike_name)
+        
+    result = {
+        "set_files": good_files,
+        "txt_files": good_txt_files,
+        "basenames": good_basenames,
+        "spike_files": good_spike_files,
+        "unit": units
+    }
+    return result
+
+def to_string(str_info):
+    """Alternative string representation should be prettier."""
+    all_str_info = []
+    for i in range(len(str_info["set_files"])):
+        b_str = "{}: \n\tSpk {}\n\tUnt {}\n\tPos {}".format(
+            i, str_info["spike_files"][i], str_info["unit"][i], 
+            str_info["txt_files"][i])
+        all_str_info.append(b_str)
+    return "\n".join(all_str_info)
 
 def main(excel_loc, data_dir):
     info = parse_excel(excel_loc)
-    print(info)
-    files = find_files(info, data_dir)
-    print(files)
+    result = find_files(info, data_dir)
     missing_files = []
     for f in info["FileName"]:
-        if f not in files:
+        if f not in result["basenames"]:
             missing_files.append(f)
-    if len(files) != len(info):
+    result_avg_len = (
+        len(result["set_files"]) + 
+        len(result["txt_files"]) + 
+        len(result["spike_files"])) / 3
+    if result_avg_len != len(info):
         raise ValueError(
-            "Failed to find some files: found {} of {}, missing {}".format(
-                len(files), len(info), missing_files))
+            "Failed to find some files:" +
+            "found set:{}, txt:{}, spike:{} of {}, missing {}".format(
+                len(result["set_files"]), len(result["txt_files"]), 
+                len(result["spike_files"]), len(info), missing_files)
+        )
+    str_v = to_string(result)
+    with open("f_info.txt", "w") as f:
+        f.write(str_v)
+    return result
 
 if __name__ == "__main__":
     excel_loc = r"E:\Pawel\ALL UNITS DATA.xlsx"
