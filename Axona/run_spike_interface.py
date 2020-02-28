a6709bae2ac36231eead45bb2ff188196386db73
Sean Martin <martins7@tcd.ie>
2020-02-28 18:44:56 +0000

    Updated spike interface run to use folder loc The folder is where the raw data is

--- Axona/run_spike_interface.py
+++ Axona/run_spike_interface.py
@@ -13,18 +13,17 @@ import numpy as np
 
 from channel_map import write_prb_file
 
-def list_extractors():
-    print("Available recording extractors", se.installed_recording_extractor_list)
-    print("Available sorting extractors", se.installed_sorting_extractor_list)
+def list_sorters():
+    """Print a list of spikeinterface sorters."""
     print('Available sorters', ss.available_sorters())
     print('Installed sorters', ss.installed_sorter_list)
 
-def print_default_params():
-    print(ss.get_default_params('mountainsort4'))
-    print(ss.get_default_params('klusta'))
-    print(ss.get_default_params('spykingcircus'))
-
 def custom_default_params_list(sorter_name, check=False):
+    """
+    Get a dictionary of params for a sorter.
+    
+    if check if True just return default.
+    """
     default_params = ss.get_default_params(sorter_name)
     if check:
         default_params = default_params
@@ -33,35 +32,48 @@ def custom_default_params_list(sorter_name, check=False):
     return default_params
 
 def run(location, sorter="klusta", output_folder="result", 
-        verbose=False, **sorting_kwargs):
+        verbose=False, view=False, **sorting_kwargs):
+    """
+    Run spike interface on a _shuff.bin file.
+
+    if verbose is True prints more information.
+
+    """
+    # Do setup
     print("Starting the sorting pipeline from bin data on {}".format(
         os.path.basename(location)))
+    in_dir = os.path.dirname(location)
+    o_dir = os.path.join(in_dir, output_folder)
+    probe_loc = os.path.join(o_dir, "channel_map.prb")
+    
+    # Load the recording data
     recording = se.BinDatRecordingExtractor(
         file_path=location, offset=16, dtype=np.int16,
         sampling_frequency=48000, numchan=64)
-    # get_info(recording)
-    # exit(-1)
+    recording_prb = recording.load_probe_file(probe_loc)
+    get_info(recording, probe_loc)
 
-    # This bit loads a probe file
-    recording_prb = recording.load_probe_file('channel_map.prb')
-    get_info(recording)
+    # Plot a trace of the raw data
+    t_len = 10
+    o_loc = os.path.join(o_dir, "trace_" + str(t_len) + "s.png")
+    print("Plotting a {}s trace of the raw data to {}".format(
+        t_len, o_loc))
+    w_ts = sw.plot_timeseries(recording_prb, trange=[0, t_len])
+    plt.savefig(o_loc, dpi=200)
 
-    # Plot a bit of the raw data
-    print("Plotting a trace of the raw data to t5s.png")
-    w_ts = sw.plot_timeseries(recording_prb, trange=[0, 5])
-    plt.savefig("t5s.png", dpi=200)
-
-    # Do pre-processing pipeline
+    # Do the pre-processing pipeline
     print("Running preprocessing")
     recording_f = st.preprocessing.bandpass_filter(
         recording_prb, freq_min=300, freq_max=6000)
-    recording_rm_noise = st.preprocessing.remove_bad_channels(
-        recording_f, bad_channel_ids=[
+    bad_chans = [
             i for i in range(3, 63, 4) 
             if i in recording_f.get_channel_ids()]
-    )
+    print("Removing {}".format(bad_chans))
+    recording_rm_noise = st.preprocessing.remove_bad_channels(
+        recording_f, bad_channel_ids=bad_chans)
     print('Channel ids after preprocess:',
           recording_rm_noise.get_channel_ids())
+    preproc_recording = recording_rm_noise
 
     # Get sorting params and run the sorting
     params = custom_default_params_list(sorter, check=False)
@@ -70,14 +82,15 @@ def run(location, sorter="klusta", output_folder="result",
     print("Running {} with parameters {}".format(
         sorter, params))
     sorted_s = ss.run_sorter(
-        sorter, recording_rm_noise, 
-        grouping_property="group", output_folder=output_folder,
+        sorter, preproc_recording, 
+        grouping_property="group", output_folder=o_dir,
         parallel=True, verbose=verbose, **params)
     
     # Some validation statistics
-    snrs = st.validation.compute_snrs(sorted_s, recording_rm_noise)
+    snrs = st.validation.compute_snrs(sorted_s, preproc_recording)
     isi_violations = st.validation.compute_isi_violations(sorted_s)
-    isolations = st.validation.compute_isolation_distances(sorted_s, recording)
+    isolations = st.validation.compute_isolation_distances(
+        sorted_s, preproc_recording)
 
     print('SNR', snrs)
     print('ISI violation ratios', isi_violations)
@@ -87,16 +100,24 @@ def run(location, sorter="klusta", output_folder="result",
     sorting_curated_snr = st.curation.threshold_snr(
         sorted_s, recording, threshold=5, threshold_sign='less')
     snrs_above = st.validation.compute_snrs(
-        sorting_curated_snr, recording_rm_noise)
+        sorting_curated_snr, preproc_recording)
 
     print('Curated SNR', snrs_above)
-    get_sort_info(sorting_curated_snr, recording_rm_noise)
+    get_sort_info(sorting_curated_snr, preproc_recording, o_dir)
 
     # Export the result to phy for manual curation
+    phy_out = os.path.join(in_dir, "phy")
     st.postprocessing.export_to_phy(
-        recording, sorting_curated_snr, output_folder='phy',
-        grouping_property='group')
-    subprocess.run(["phy", "template-gui", "phy/params.py"])
+        recording, sorting_curated_snr, 
+        output_folder=phy_out, grouping_property='group')
+    
+    phy_final = os.path.join(phy_out, "params.py")
+    if view:
+        subprocess.run(["phy", "template-gui", phy_final])
+    else:
+        print(
+            "To view the data in phy, run: phy template-gui {}".format(
+            phy_final))
 
     # If you need to process the data further!
     # sorting_phy_curated = se.PhySortingExtractor("phy")
@@ -105,40 +126,42 @@ def run(location, sorter="klusta", output_folder="result",
     # See the ending part of 
     # https://github.com/SpikeInterface/spiketutorials/blob/master/Spike_sorting_workshop_2019/SpikeInterface_Tutorial.ipynb
 
-def get_sort_info(sorting, recording):
+def get_sort_info(sorting, recording, out_loc):
     unit_ids = sorting.get_unit_ids()
-    spike_train = sorting.get_unit_spike_train(unit_id=unit_ids[0])
-
     print("Found", len(unit_ids), 'units')
     print('Unit ids:', unit_ids)
-    w_rs = sw.plot_rasters(sorting, trange=[0, 5])
-    plt.savefig("raster5s.png", dpi=200)
 
-    print('Spike train of first unit:', 
-        np.asarray(spike_train) / 48000)
+    spike_train = sorting.get_unit_spike_train(unit_id=unit_ids[0])
+    print('Spike train of first unit:', np.asarray(spike_train) / 48000)
+
+    # Spike raster plot
+    t_len = 10
+    o_loc = os.path.join(out_loc, "raster_" + str(t_len) + "s.png")
+    print("Saving {}s rasters to {}".format(t_len, o_loc))
+    w_rs = sw.plot_rasters(sorting, trange=[0, t_len])
+    plt.savefig(o_loc, dpi=200)
 
     # See also spiketoolkit.postprocessing.get_unit_waveforms
     num_samps = min(5, len(unit_ids))
     w_wf = sw.plot_unit_waveforms(
         sorting=sorting, recording=recording, unit_ids=unit_ids[:num_samps])
-    plt.savefig("5waveforms.png", dpi=200)
+    o_loc = os.path.join(out_loc, "waveforms_" + str(num_samps) + ".png")
+    print("Saving {} waveforms to {}".format(num_samps, o_loc))
+    plt.savefig(o_loc, dpi=200)
 
 def get_info(recording, prb_fname="channel_map.prb"):
-    print("Recording information:")
     fs = recording.get_sampling_frequency()
     num_chan = recording.get_num_channels()
+    recording_prb = recording.load_probe_file(prb_fname)
 
+    print("Recording information:")
     print('Sampling frequency:', fs)
     print('Number of channels:', num_chan)
-
-    recording_prb = recording.load_probe_file(prb_fname)
-    # print('Original channels:', recording.get_channel_ids())
-    print('Channels after loading the probe file:', recording_prb.get_channel_ids())
+    print(
+        'Channels after loading the probe file:', 
+        recording_prb.get_channel_ids())
     print('Channel groups after loading the probe file:',
         recording_prb.get_channel_groups())
-    # print(recording_prb.get_channel_locations())
-
-    return recording_prb
 
 def compare_sorters(sort1, sort2):
     comp_KL_MS4 = sc.compare_two_sorters(
@@ -163,13 +186,15 @@ if __name__ == "__main__":
     # list_extractors()
     # print_default_params()
     print("Starting to run spike interface!")
-    in_dir = r"G:\Ham Data\A10_CAR-SA2\CAR-SA2_20200109_PreBox"
+    in_dir = r"G:\Ham\A10_CAR-SA2\CAR-SA2_20200109_PreBox"
     fname = "CAR-SA2_2020-01-09_PreBox_shuff.bin"
     location = os.path.join(in_dir, fname)
     out_folder = "results_tet12_klusta"
+    out_loc = os.path.join(in_dir, out_folder, "channel_map.prb")
+    os.makedirs(os.path.dirname(out_loc), exist_ok=True)
 
     # Remove this if you don't want a new channel_map
-    write_prb_file(tetrodes_to_use=[12])
+    write_prb_file(tetrodes_to_use=[12], out_loc=out_loc)
 
     # grouping_property = group controls whether clustering is ran
     # per tetrode, or on the entire recording
