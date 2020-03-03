3d84c4eff4b2716b703bb04deaa3c96058961c76
Sean Martin <martins7@tcd.ie>
2020-03-03 14:19:22 +0000

    Read bin uses shape in memmap

--- Axona/read_bin_to_plot.py
+++ Axona/read_bin_to_plot.py
@@ -13,9 +13,12 @@ def int16toint8(value):
     return value
 
 def read_shuff_bin(location, channels=[0], times=[1], fname="fig.png"):
-    data = np.memmap(location, dtype="int16", offset=16, mode="r")
+    bytes_size = os.path.getsize(location)
+    one_channel_len = int((bytes_size - 16) / 128)
     num_chans = 64
-    one_channel_len = int(data.size / num_chans)
+    data = np.memmap(
+        location, dtype="int16", offset=16, mode="r", 
+        shape=(num_chans, one_channel_len))
     ts = np.arange(0, 1, 0.02)
     out_data = np.zeros(
         shape=(len(channels), len(times), len(ts)), dtype=np.int16)
@@ -25,9 +28,9 @@ def read_shuff_bin(location, channels=[0], times=[1], fname="fig.png"):
     for i, channel in enumerate(channels):
         for j, time in enumerate(times):
             s_mid = int(time * 48000)
-            start_idx = (channel * one_channel_len) + (s_mid - s_back)
+            start_idx = s_mid - s_back
             end_idx = start_idx + len(ts)
-            one_channel = data[start_idx:end_idx]
+            one_channel = data[channel, start_idx:end_idx]
             out_data[i][j] = one_channel
     fig, axes = plt.subplots(len(channels), figsize=(5, 10))
     for row, ax in zip(out_data, axes):
@@ -46,11 +49,11 @@ def read_shuff_bin(location, channels=[0], times=[1], fname="fig.png"):
 if __name__ == "__main__":
     location = r"C:\Users\smartin5\Recordings\Raw\2min\CS1_18_02_open_2_bin_shuff.bin"
     location = r"G:\Ham\A10_CAR-SA2\CAR-SA2_20200109_PreBox\CAR-SA2_2020-01-09_PreBox_shuff.bin"
-    location = r"F:\CAR-SA4_20200301_PreBox\CAR-SA4_2020-03-01_PreBox.bin"
-    tetrode = 1
+    # location = r"F:\CAR-SA4_20200301_PreBox\CAR-SA4_2020-03-01_PreBox.bin"
+    tetrode = 12
     channels = [4 * (tetrode-1) + i for i in range(4)]
     times = []
-    n_times = 1
+    n_times = 10
     # spike_names = "CAR-SA4_2020-03-01_PreBox_12_c1_times.txt"
     # with open(os.path.join(os.path.dirname(location), spike_names), "r") as f:
     #     for i in range(n_times):
@@ -64,7 +67,7 @@ if __name__ == "__main__":
     ns.load(
         os.path.join(
             os.path.dirname(location), 
-            "CAR-SA4_2020-03-01_PreBox.1"), "Axona")
+            "CAR-SA2_2020-01-09_PreBox.12"), "Axona")
     ns.set_unit_no(unit_no=1)
     times = []
     for i in range(n_times):
@@ -72,4 +75,4 @@ if __name__ == "__main__":
         times.append(t)
     nc_plot.wave_property(ns.wave_property())
     plt.savefig("wave.png")
-    read_shuff_bin(location, channels, times, fname="test12_1_ac_new.png")
+    read_shuff_bin(location, channels, times, fname="test12_1_ac_new2.png")
